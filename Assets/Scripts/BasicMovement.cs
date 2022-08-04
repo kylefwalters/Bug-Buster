fecea744b29d43bc7c42b652c2dcfe7c48c3fff6
jagamaru115 <110244534+jagamaru115@users.noreply.github.com>
2022-08-04 23:45:36 +0900

    カメラ操作

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -30,6 +30,16 @@ public class BasicMovement : MonoBehaviour
         {
             transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
         }
-        
+
+        Camera cam = Camera.main;
+        // Converting the mouse position to a point in 3D-space
+        Vector3 point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        // Using some math to calculate the point of intersection between the line going through the camera and the mouse position with the XZ-Plane
+        float t = cam.transform.position.y / (cam.transform.position.y - point.y);
+        Vector3 finalPoint = new Vector3(t*(point.x - cam.transform.position.x) + cam.transform.position.x, transform.position.y, t*(point.z - cam.transform.position.z) + cam.transform.position.z);
+        //Rotating the object to that point
+        transform.LookAt(finalPoint, Vector3.up);
+
+
     }
 }
